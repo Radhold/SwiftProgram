55fdfb103201263966d49708518349a7accae33f
radhold <fomenko.yaroslav@gmail.com>
2021-05-29 23:18:57 +0300

    Initial Commit

---
+++ Weekday FinderS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weekday FinderS
+//
+//  Created by Yaroslav Fomenko on 29.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
